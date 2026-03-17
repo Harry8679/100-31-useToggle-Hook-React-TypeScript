009b19cda2b488f0cd34768c1ef9b8695c70085c
Harry7986 <emarh.harry.code@gmail.com>
2026-03-17 23:25:13 +0100

    types 2

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// Types pour toggle - AUCUN ANY
+
+export interface AccordionItem {
+  id: string;
+  title: string;
+  content: string;
+  icon: string;
+}
+
+export interface Feature {
+  id: string;
+  name: string;
+  description: string;
+  enabled: boolean;
+}
